996ee509780a5d91df06834a47960614832d7b0a
Olivera Micic <omicic@gmail.com>
2021-04-16 15:17:25 +0200

    primer sa https://www.danilovesovic.com/kursevi/js-game/

---
+++ main.js
@@ -0,0 +1,58 @@
+(function () {
+        let startBtn = $('.start-btn');
+        let mainInput = $('.main-input');
+        let allLines = $('.line');
+
+        startBtn.on('click', startGame);
+
+        function startGame() {
+
+            $(this).hide();
+            //console.log(a);
+            let speed = 1;
+            let textLength = 3;
+            let typingWords = words.filter(word => word.length == textLength);
+            let lvl = 6;
+
+            console.log(typingWords);
+
+            function insertSpans(){
+                for (let i = 0; i<allLines.length; i++){
+                    let rand = Math.floor(Math.random() * 20);
+                    if(rand <= lvl){
+                        let text = chooseText();
+                        $(allLines[i]).append(`<span>${text}</span>`)
+                    }
+                }
+            }
+            insertSpans();
+
+            function chooseText(){
+                let rand = Math.floor(Math.random() * typingWords.length);
+                let savedText = typingWords[rand];
+                typingWords.splice(rand,1);
+                return savedText;
+            }
+
+            // animacija spanova
+            let moveAll = setInterval(function (){
+                let allSpans = $('span');
+                allSpans.css({
+                    left : `+=`+speed
+                })
+
+                //testiranje
+                $.each(allSpans, (index,el)=>{
+                    let position = $(el).position().left;
+                    if(position > 850)[
+                        clearAllIntervals()
+                    ]
+                })
+            },5)
+
+            function clearAllIntervals(){
+                clearInterval(moveAll);
+        }
+
+        }
+    })()
